d7bb3f63d1374b5b32dca5976896bf4562bb8b98
ohadbenisti <155305736+ohadbenisti@users.noreply.github.com>
2024-02-11 17:10:01 +0200

    Create Read Me cartCalc.py

---
+++ Read Me cartCalc.py
@@ -0,0 +1,3 @@
+This code could get a dictionary that contains some products in the place o f keys, and the prices in the place of values,
+and an other list with the item needed to buy, and the amount needed.
+The code print the iten that missing and the total price in the end.
